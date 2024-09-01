9a17de8c93df87349f4797628c416af9fca56016
Panos <pfotopoulos1996@gmail.com>
2024-09-02 00:28:00 +0300

    .

--- Supervised Learning/Documentation for Implementation/Scikit-Learn/TTS-Regression/TTSLogisticRegression.py
+++ Supervised Learning/Documentation for Implementation/Scikit-Learn/TTS-Regression/TTSLogisticRegression.py
@@ -24,10 +24,42 @@ from sklearn.linear_model import LogisticRegression
 log_model = LogisticRegression()
 log_model.fit(scaled_X_train,y_train)
 
-log_model.coef_
-
+coef = log_model.coef_
+print(f'my coef are: {coef}')
+'''
+My coef are: [[-0.94953524  3.45991194]]* and the dataframe with labels Age/physical_score. You can notice that the coef for age is negative
+and thats true because as as the age is getting higher/older that mean that more people are failing the test. 
+And the coef for physcial is postive that means that they oods increase to pass the test
+'''
 y_pred = log_model.predict(scaled_X_test,)
 #print (y_pred)
 
 y_pred_prob = log_model.predict_proba(scaled_X_test,)
 #print (y_pred_prob)
+
+from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
+
+accuracy_score(y_test,y_pred)
+confusion_matrix(y_test,y_pred)
+'''
+True Possitive   False Negative
+False Possitive  True Negative
+'''
+my_report = classification_report(y_test,y_pred)
+
+print(f'My classfication reports are: {my_report}')
+
+from sklearn.metrics import precision_score,recall_score
+
+my_precision_score = precision_score(y_test,y_pred)
+print(f'My classfication prcision score is: {my_precision_score}')
+
+my_recall_score = recall_score(y_test,y_pred)
+print(f'My classfication recall score is: {my_recall_score}')
+
+
+In_what_class_do_this_belong = log_model.predict_proba(scaled_X_test)[0]
+print(f'The probability "%" to be class 0 or 1 is {In_what_class_do_this_belong}')
+
+what_class_actualy_belongs_at = y_test[0]
+print(f'Actually belongs at class: {what_class_actualy_belongs_at}')
